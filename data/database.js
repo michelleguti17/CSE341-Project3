14d179651ef49379805c1a865b45ecdde605e24d
michelleguti17 <gut20014@byui.edu>
2023-11-17 09:59:22 -0500

    database

--- data/database.js
+++ data/database.js
@@ -2,37 +2,30 @@ const dotenv = require('dotenv');
 dotenv.config();
 const MongoClient = require('mongodb').MongoClient;
 
-let db;
+let _db;
 
 const initDb = (callback) => {
-    if(db){
-        console.log('Db is already initialized');
-        return callback(null,db);
-    }
-
-    MongoClient.connect(process.env.MONGODB_URI)
-    .then((client)=>{
-        db = client
-        callback(null, db);
-
-        })
-        .catch((err) => {
-            callback(err);
-        });
-
+  if (_db) {
+    console.log('Db is already initialized!');
+    return callback(null, _db);
+  }
+  MongoClient.connect(process.env.MONGODB_URI)
+    .then((client) => {
+      _db = client;
+      callback(null, _db);
+    })
+    .catch((err) => {
+      callback(err);
+    });
 };
 
-
-const getDatabase =() => {
-    if(!db){
-        throw Error('Db is not initialized');
-        
-    }
-    return db;
-
-
-
+const getDatabase = () => {
+  if (!_db) {
+    throw Error('Db not initialized');
+  }
+  return _db;
 };
+
 module.exports = {
     initDb,
     getDatabase
