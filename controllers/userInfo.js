72a384525e64e65eb7d88c8d7ec223104978ef9c
michelleguti17 <gut20014@byui.edu>
2023-11-17 09:56:43 -0500

    removing object

--- controllers/userInfo.js
+++ controllers/userInfo.js
@@ -18,9 +18,6 @@ const getAll = (req, res) => {
   };
   
   const getSingle = (req, res) => {
-    if (!ObjectId.isValid(req.params.id)) {
-      res.status(400).json('Must use a valid contact id to find a contact.');
-    }
     const userId = new ObjectId(req.params.id);
     mongodb
       .getDatabase()
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 router.get('/', (req, res)=>{//#swagger.tags=['Hello World']
 res.send("Hello World!")});
-router.use('/', require('./swagger'));
 router.use('/userInfo', require('./userInfo'));
 
 module.exports = router;
