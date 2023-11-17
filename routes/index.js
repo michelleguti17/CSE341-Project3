dc57cc059a33e3e44dc049308af1f171dc613f91
michelleguti17 <gut20014@byui.edu>
2023-11-17 09:50:40 -0500

    add swagger

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 router.use('/', require('./swagger'));
 router.get('/', (req, res)=>{//#swagger.tags=['Hello World']
 res.send("Hello World!")});
-
+router.use('/', require('./swagger'));
 router.use('/userInfo', require('./userInfo'));
 
 module.exports = router;
